7af0fce89647b1c5553303fcd5f7457dc6a8e271
ryanpepper <ryan.pepper1@gmail.com>
2017-03-02 17:00:28 +0000

    Drop some stuff

--- intermediate-cython/gravcython.pyx
+++ intermediate-cython/gravcython.pyx
@@ -1,38 +1,30 @@
 import matplotlib.pyplot as plt
-cimport numpy as np
-cimport cython
 import numpy as np
 import time
 from cython.parallel import prange
+cimport numpy as np
+cimport cython
 
-from libc.math cimport sin
-from libc.math cimport cos
-from libc.math cimport atan2
-from libc.math cimport M_PI
 begin = time.time()
 
-# Add type annotations, turn off bounds checking and wraparound
-
-@cython.boundscheck(False) # Turns off IndexError type warnings - e.g. a = [1, 2, 3]; a[5]
-@cython.wraparound(False) # Turns off Python a[-1] indexing - will segfault.
-@cython.overflowcheck(False) # Check that no integer overflows occur with arithmetic operations
-@cython.initializedcheck(False) # Checks that memory is initialised when passed in
-cdef F(np.ndarray[double, ndim=2] f, np.ndarray[double, ndim=2] r, np.ndarray[double, ndim=1] q):
+cdef F(np.ndarray[double, ndim=2] f,
+       np.ndarray[double, ndim=2] r,
+       np.ndarray[double, ndim=1] q):
     cdef int i, j
     cdef double dx, dy, F, theta
     cdef int N
     N = len(f)
     f[:] = 0
-    for i in prange(N, nogil=True):
+    for i in range(N):
         for j in range(N):
             if i != j:
                 dx = r[i, 0] - r[j, 0]
                 dy = r[i, 1] - r[j, 1]
-                theta = atan2(dy, dx)
-                F = q[i]*q[j]/(4*M_PI*8.854e-12*(dx*dx + dy*dy))
-                f[i, 0] += F*cos(theta)
-                f[i, 1] += F*sin(theta)
-    
+                theta = np.arctan2(dy, dx)
+                F = q[i]*q[j]/(4*np.pi*8.854e-12*(dx*dx + dy*dy))
+                f[i, 0] += F*np.cos(theta)
+                f[i, 1] += F*np.sin(theta)
+
 N = 1000
 np.random.seed(0)
 r = np.random.uniform(0, 1, (N, 2))
@@ -51,4 +43,3 @@ while t < T:
 
 end = time.time()
 print(end - begin)
-
